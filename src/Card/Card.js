0176c007eab627f254b6a0efc44038adbd03137a
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-09-06 11:31:04 -0400

    first commit. First pass at the Card component

---
+++ src/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card(props) {
+    return (
+        <div className='Card'>
+            <button
+                type='button'
+                onClick={() => props.onClickDelete(props.id)}
+            >delete</button>
+
+            <h3>{props.name}</h3>
+            <p>{props.modified}</p>
+        </div>   
+    )
+}
+
+Card.propTypes = {
+    onClickDelete: () => {}
+}
