25ca093fb5c179aab02ad25c9e4917adee421697
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-09-16 12:36:12 -0400

    added selected Folder css

--- src/Folder/Folder.js
+++ src/Folder/Folder.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Folder.css';
 
 
 export default function Folder(props) {
-    return (
-            <Link to={'/folder/' + props.id} >
+        return (
+            <NavLink to={'/folder/' + props.id} >
                 <div className='Folder'>
                     <h3>{props.name}</h3>
                 </div> 
-            </Link> 
- 
+            </NavLink> 
     )
 }
